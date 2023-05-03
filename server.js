188a963c455aa8b55ec6cee931a37e77f046a980
Joshua Makula <makulajosh@gmail.com>
2023-05-03 16:12:58 +0300

    Redirect routes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
  const fs = require('fs')
 
 //  Create a server
-
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
 
@@ -14,12 +13,22 @@ const server = http.createServer((req, res) => {
     switch(req.url){
         case '/':
             path += 'index.html'
+            res.statusCode = 200
             break
         case '/about':
             path += 'about.html'
+            res.statusCode = 200
+            break
+        
+            // re-direct
+        case '/about-me':
+            res.statusCode = 301
+            res.setHeader('Location', '/about')
+            res.end()
             break
         default:
             path += '404.html'
+            res.statusCode = 404
             break
     }
     
