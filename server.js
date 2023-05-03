2422d9831cda3994a44221b53da1c161ec2ef0e1
Joshua Makula <makulajosh@gmail.com>
2023-05-03 17:05:02 +0300

    NPM

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
  const http = require('http')
  const fs = require('fs')
+ const _ = require("lodash")
 
 //  Create a server
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
+    
+    // lodash
+    const num = _.random(0, 20)
+    console.log(num)
+
+    const greet = _.once(() => {
+        console.log('hello')
+    })
+
+    greet()
+    greet()
 
     // Set header Content type
     res.setHeader('Content-Type', 'text/html')
