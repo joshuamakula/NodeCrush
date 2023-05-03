9484c1a479d185eb2a98176973141bf57c1f7252
Joshua Makula <makulajosh@gmail.com>
2023-05-03 16:00:34 +0300

    Simple routing

--- server.js
+++ server.js
@@ -8,9 +8,23 @@ const server = http.createServer((req, res) => {
 
     // Set header Content type
     res.setHeader('Content-Type', 'text/html')
+
+    // routing path
+    let path = './views/'
+    switch(req.url){
+        case '/':
+            path += 'index.html'
+            break
+        case '/about':
+            path += 'about.html'
+            break
+        default:
+            path += '404.html'
+            break
+    }
     
     // send an html file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err){
             console.log(err)
         } else {
