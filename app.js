6160086982027798f8bf70e5d826b97e774323fb
Joshua Makula <makulajosh@gmail.com>
2023-05-04 18:04:19 +0300

    Outputting document in view

--- app.js
+++ app.js
@@ -23,56 +23,23 @@ app.listen(3000)
 app.use(express.static('public'))
 app.use(morgan('dev'))
 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'Another good Blog',
-        snippet: 'About a new blog',
-        body: 'More about the new blog'
-    })
-
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-// get mongodb data
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-// get single data
-app.get('/single-blog', (req, res) => {
-    Blog.findById('6453c4e48bd67182518440b8')
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
 
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet, consectetur'},
-        {title: 'Maria finds stars', snippet: 'Lorem ipsum dolor sit amet, consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet, consectetur'},
-    ]
-    res.render('index', {title: 'Home', blogs})
+    res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'})
+})
+
+app.get('/blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.render('index', {title: 'All Blogs', blogs: result})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 
 app.get('/blogs/create', (req, res) => {
