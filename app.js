af85d257f35826b55431dd2fb520761ead5fb776
Joshua Makula <makulajosh@gmail.com>
2023-05-04 18:06:16 +0300

    Sort output

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/blogs', (req, res) => {
-    Blog.find()
+    Blog.find().sort({ createdAt: -1 })
         .then((result) => {
             res.render('index', {title: 'All Blogs', blogs: result})
         })
