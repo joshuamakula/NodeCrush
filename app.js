83ebb4fea387a8f8aa8af35282a4c22a34205906
Joshua Makula <makulajosh@gmail.com>
2023-05-04 17:31:44 +0300

    getting and saving data to mongodb

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./model/blogs')
+const { result } = require('lodash')
 
 // express app
 const app = express()
+
+// Connect to mongobd
+const dbURI = 'mongodb+srv://admin:admin@cluster0.w6udrr2.mongodb.net/crush?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err))
 
 //  register view engine
 app.set('view engine', 'ejs')
@@ -10,8 +19,26 @@ app.set('view engine', 'ejs')
 // listen for request
 app.listen(3000)
 
-// middleware
+// middleware & static files
+app.use(express.static('public'))
 app.use(morgan('dev'))
+
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'New Blog',
+        snippet: 'About a new blog',
+        body: 'More about the new blog'
+    })
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 app.get('/', (req, res) => {
     const blogs = [
